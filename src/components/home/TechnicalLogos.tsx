0f2c8eed297a0700cad0a8993204c0f97e2de951
Ashfak Md Shibli <shibli.emon@gmail.com>
2025-09-16 12:34:03 -0400

    Revert "Add PHP and JavaScript to tech stack Languages & ML section"
    This reverts commit a75ce255b83a200465160ab2fa3926871858b050.

--- src/components/home/TechnicalLogos.tsx
+++ src/components/home/TechnicalLogos.tsx
@@ -15,9 +15,7 @@ import {
   SiGit,
   SiJira,
   SiPytorch,
-  SiDocker,
-  SiPhp,
-  SiJavascript
+  SiDocker
 } from "react-icons/si";
 
 const techGroups = [
@@ -25,8 +23,6 @@ const techGroups = [
     name: "Languages & ML",
     items: [
       { icon: SiPython, color: "#3776AB", name: "Python" },
-      { icon: SiJavascript, color: "#F7DF1E", name: "JavaScript" },
-      { icon: SiPhp, color: "#777BB4", name: "PHP" },
       { icon: SiSwift, color: "#F05138", name: "Swift" },
       { icon: SiDotnet, color: "#239120", name: "C#" },
       { icon: SiCplusplus, color: "#00599C", name: "C++" }
